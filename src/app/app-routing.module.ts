472dc41b662cdb6692aeb580c47a86764faa4ade
Erkan Ertürk <erkanerturk096@gmail.com>
2019-04-22 00:35:07 +0300

    theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   { path: 'products', component: ProductComponent },
+  { path: 'product-add-1', component: ProductComponent },
+  { path: 'product-add-2', component: ProductComponent },
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products/category/:categoryId', component: ProductComponent },
 ];
